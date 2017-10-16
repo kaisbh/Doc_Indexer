614472a3fd40d835d112d0c81ad85493c5abd319
kaisbh <kaisbh2007@gmail.com>
2017-10-16 23:52:11 +0200

    First commit

---
+++ Doc_Indexer/src/LuceneIndexDocument.java
@@ -0,0 +1,59 @@
+ 
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+ 
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.FSDirectory;
+ 
+public class LuceneIndexDocument
+{
+    private static final String INDEX_DIR = "\\\\fs1pedago.pedago.cri.u-bordeaux2.fr\\Comptes_LDAP\\kbenhadjhmid\\Desktop\\tp\\MyIndex";
+ 
+    public static void main(String[] args) throws Exception
+    {
+        IndexWriter writer = createWriter();
+        List<Document> documents = new ArrayList<>();
+         
+        Document document1 = createDocument(1, "Gayo", "Diallo", "howtodoinjava.com");
+        documents.add(document1);
+         
+        Document document2 = createDocument(2, "Jean", "Daniel", "example.com");
+        documents.add(document2);
+        
+        Document document3 = createDocument(2, "kais", "BenHadj", "example.com");
+        documents.add(document2);
+         
+        //Let's clean everything first
+        writer.deleteAll();
+         
+        writer.addDocuments(documents);
+        writer.commit();
+        writer.close();
+    }
+ 
+    private static Document createDocument(Integer id, String firstName, String lastName, String website)
+    {
+        Document document = new Document();
+        document.add(new StringField("id", id.toString() , Field.Store.YES));
+        document.add(new TextField("firstName", firstName , Field.Store.YES));
+        document.add(new TextField("lastName", lastName , Field.Store.YES));
+        document.add(new TextField("website", website , Field.Store.YES));
+        return document;
+    }
+ 
+    private static IndexWriter createWriter() throws IOException
+    {
+        FSDirectory dir = FSDirectory.open(Paths.get(INDEX_DIR));
+        IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
+        IndexWriter writer = new IndexWriter(dir, config);
+        return writer;
+    }
+}
---
+++ Doc_Indexer/src/MyAnalyzer.java
@@ -0,0 +1,50 @@
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.core.WhitespaceTokenizer;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.Analyzer.TokenStreamComponents;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.util.Version;
+
+public class MyAnalyzer extends Analyzer {
+ 
+   private Version matchVersion;
+   
+   public MyAnalyzer(Version matchVersion) {
+     this.matchVersion = matchVersion;
+   }
+ 
+   @Override
+   protected TokenStreamComponents createComponents(String fieldName) {
+     return new TokenStreamComponents(new StandardTokenizer());
+   }
+   
+   public static void main(String[] args) throws IOException {
+     // text to tokenize
+     final String text = "This is a demo of the TokenStream API";
+     
+     Version matchVersion = Version.LUCENE_6_6_0; // Substitute desired Lucene version for XY
+     MyAnalyzer analyzer = new MyAnalyzer(matchVersion);
+     TokenStream stream = analyzer.tokenStream("field", new StringReader(text));
+     
+     // get the CharTermAttribute from the TokenStream
+     CharTermAttribute termAtt = stream.addAttribute(CharTermAttribute.class);
+ 
+     try {
+       stream.reset();
+     
+       // print all tokens until stream is exhausted
+       while (stream.incrementToken()) {
+         System.out.println(termAtt.toString());
+       }
+     
+       stream.end();
+     } finally {
+       stream.close();
+     }
+   }
+ }
+ 
